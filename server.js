f30b6c425a3e23912739b47b44c4460dabf0982c
Chris Jones <chrisjonesdesign85@gmail.com>
2019-08-02 23:30:39 -0400

    CRUD for actions complete

--- server.js
+++ server.js
@@ -88,4 +88,66 @@ server.put('/', (req, res) => {
 });
 
 
+
+//ACTIONS
+server.post('/actions', (req, res) => {
+    const actionInfo = req.body;
+    actionModel.insert(actionInfo)
+        .then(action => {
+            res.status(201).json({ success: true, action });
+        })
+        .catch(err => {
+            res.status(404).json({
+                success: false,
+                message: 'Cannot find the project you are looking for.'
+            });
+        })
+});
+
+
+//DELETE
+server.delete('/actions/:id', (req, res) => {
+    const id = req.params.id;
+    actionModel.remove(id)
+        .then(count => {
+            if (count) {
+                res.status(201).end()
+            } else {
+                res.status(404).json({
+                    message: 'There is no action with the specified id.'
+                });
+            }
+        })
+        .catch(err => res.status(500)
+            .json({
+                message: 'Error deleting that id'
+            })
+        )
+})
+
+
+// PUT
+server.put('/actions/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    actionModel.update(id, changes)
+        .then(update => {
+            if (update) {
+                res.status(200)
+                    .json({ success: true, update })
+            } else {
+                res.status(404)
+                    .json({
+                        message: 'Something went wrong updating the action.'
+                    })
+            }
+        })
+        .catch(err => res.status(500)
+            .json({
+                message: 'There is no action with that id.'
+            })
+        )
+})
+
+
 module.exports = server;
